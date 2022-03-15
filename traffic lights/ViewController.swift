7afac23d92f4abae153b0e16f3c6eb46f4af3658
Sergey <kolesnikov.sergey.al@gmail.com>
2022-03-15 10:33:29 +0300

    changing the value of the button changed the font. fixed

--- traffic lights/ViewController.swift
+++ traffic lights/ViewController.swift
@@ -11,14 +11,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var trafficLightRed: UIView!
     @IBOutlet weak var trafficLightsYellow: UIView!
     @IBOutlet weak var trafficLightsGreen: UIView!
+    @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         trafficLightRed.alpha = 0.3
         trafficLightsGreen.alpha = 0.3
         trafficLightsYellow.alpha = 0.3
+        
+        trafficLightRed.layer.cornerRadius = 60
+        trafficLightsGreen.layer.cornerRadius = 60
+        trafficLightsYellow.layer.cornerRadius = 60
+        
+        startButton.layer.cornerRadius = 10
+        }
+
+    @IBAction func changeTrafficLightsColorPressed(_ sender: Any) {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        if trafficLightRed.alpha < 1 && trafficLightsYellow.alpha < 1 || trafficLightsGreen.alpha == 1 {
+            trafficLightRed.alpha = 1
+            trafficLightsGreen.alpha = 0.3
+        } else if trafficLightsYellow.alpha < 1 && trafficLightRed.alpha == 1 {
+            trafficLightRed.alpha = 0.3
+            trafficLightsYellow.alpha = 1
+        } else if trafficLightsYellow.alpha == 1 && trafficLightsGreen.alpha < 1 {
+            trafficLightsYellow.alpha = 0.3
+            trafficLightsGreen.alpha = 1
+        }
+        
     }
-
-
 }
 
